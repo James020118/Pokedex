a6cd0e1588d2613d6c4ab88505ee90623dcd81ac
Ximing Yang <james020118@gmail.com>
2023-08-17 16:59:08 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ximing Yang on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
